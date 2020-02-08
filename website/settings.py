b3a56148185fa71883e0087adb7b474c391b00d5
laizeqiang <916323301@qq.com>
2020-02-08 20:46:12 +0800

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'g*2#90iw7(gtgdgzlar3&7iw))pd%=hv2u!o!e)@c5$v-x5s!4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.zeqianglai.cn', '175.24.111.240']
 
 AUTH_USER_MODEL = 'account.User'
 

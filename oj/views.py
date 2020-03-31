b67d71ce478d13d05a750217ecb937ff7e292ba9
Zeqiang-Lai <916323301@qq.com>
2020-03-31 18:31:57 +0800

    fix: best submission include submissions that have not been graded

--- oj/views.py
+++ oj/views.py
@@ -76,7 +76,7 @@ class CourseAssignmentView(LoginRequiredMixin, generic.DetailView):
         if len(submissions) == 0:
             best_submission = None
         else:
-            best_submission = submissions.order_by('gradereport__grade')[0]
+            best_submission = submissions.filter(gradereport__isnull=False).order_by('gradereport__grade')[0]
         context['best_submission'] = best_submission
 
         return context

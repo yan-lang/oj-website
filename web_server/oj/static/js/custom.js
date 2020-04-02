3c9d55a10648ced35decc9fb985f9bb8b3e7d77b
Zeqiang-Lai <916323301@qq.com>
2020-04-02 21:11:47 +0800

    feat: improve ux experience

--- web_server/oj/static/js/custom.js
+++ web_server/oj/static/js/custom.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
     $("button.grade-expand").click(function () {
-        if($(this).attr("aria-expanded") === "true") {
-            $(this).children("svg.bi-chevron-down").hide()
+        if ($(this).attr("aria-expanded") === "true") {
+            $(this).children("svg.bi-chevron-down").hide();
             $(this).children("svg.bi-chevron-right").show()
         } else {
-            $(this).children("svg.bi-chevron-right").hide()
+            $(this).children("svg.bi-chevron-right").hide();
             $(this).children("svg.bi-chevron-down").show()
+        }
+    })
+});
+
+$(document).ready(function () {
+    $("a.detail-expand").click(function () {
+        if ($(this).attr("aria-expanded") === "true") {
+            $(this).children(".hide-prompt").hide();
+            $(this).children(".show-prompt").show()
+        } else {
+            $(this).children(".show-prompt").hide();
+            $(this).children(".hide-prompt").show()
         }
     })
 });
